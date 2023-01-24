2487c2a01b1aa11cc474671a7648c8dddfaf99e8
Sissom <111325589+Sissom@users.noreply.github.com>
2023-01-24 01:45:27 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,20 @@
+arr = [
+  {
+    name: "Amy",
+  },
+  {
+    name: "Jimmy",
+  },
+  {
+    name: "Johnny",
+  }
+]
+
+arr.unshift(name: "Matt")
+print arr
+
+arr.insert(4, name: "Tommy")
+print arr
+
+arr.delete_at(4)
+print arr
